96cfc598012a64847e13c2252a0b1749da710889
kenedy lima <kenedytorreslima@gmail.com>
2023-03-21 21:00:18 -0300

    trabalhando na sessão de projetos

--- src/Components/ProjetosCarrosel.jsx
+++ src/Components/ProjetosCarrosel.jsx
@@ -1,3 +1,59 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
-export default function ProjetosCarrosel() {}
+const Container = styled.div`
+  position: relative;
+  width: 60%;
+  height: 40rem;
+  margin-left: 50%;
+  transform: translateX(-50%);
+`;
+
+const ArrowCss = css`
+  position: absolute;
+  top: 50%;
+  font-size: 4.8rem;
+  color: #78350f;
+`;
+
+const PlantaContainer = styled.div`
+  ${({ planta }) => css`
+    background-image: url(${planta});
+    background-repeat: no-repeat;
+  `}
+  width: 100%;
+  height: 100%;
+  transform: rotate(90deg);
+`;
+
+const ArrowForWrapper = styled.div`
+  ${ArrowCss}
+  right: 0;
+  transform: translate(50%, -50%);
+`;
+
+const ArrowBackWrapper = styled.div`
+  ${ArrowCss}
+  transform: translate(-50%, -50%);
+`;
+export default function ProjetosCarrosel({ plantas }) {
+  return (
+    <Container>
+      <ArrowBackWrapper>
+        <ion-icon
+          className="arrow arrow-back"
+          name="arrow-back-circle-outline"
+        ></ion-icon>
+      </ArrowBackWrapper>
+      <ArrowForWrapper>
+        <ion-icon
+          className="arrow arrow-for"
+          name="arrow-forward-circle-outline"
+        ></ion-icon>
+      </ArrowForWrapper>
+      {plantas.map((planta, index) => {
+        return <PlantaContainer key={index} planta={planta}></PlantaContainer>;
+      })}
+    </Container>
+  );
+}
--- src/Projetos.jsx
+++ src/Projetos.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { SessionSubtitle, SessionTitle } from "./Sobre";
-
+import PlantaUm from "./assets/Projetos/Planta 1.png";
+import ProjetosCarrosel from "./Components/ProjetosCarrosel";
 const Container = styled.div`
   padding: 3.6rem;
   background-color: #92400e;
@@ -21,6 +22,7 @@ export default function Projetos() {
         muitos não são de minha autoria.
       </Paragraph>
       <SessionSubtitle>Projeto 1</SessionSubtitle>
+      <ProjetosCarrosel plantas={[PlantaUm]} />
     </Container>
   );
 }
